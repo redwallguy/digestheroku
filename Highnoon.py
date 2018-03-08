c5b50b09022089c32ace6a9fbca4980931f7a564
Vibhu Someshwar <vibhusomeshwar@Vibhus-MBP.fios-router.home>
2018-03-08 17:33:59 -0500

    Fixed administrative commands

--- Highnoon.py
+++ Highnoon.py
@@ -3,6 +3,7 @@ import asyncio
 import os
 import errormail
 import redis
+import imgurpackage
 
 client = discord.Client()
 
@@ -10,18 +11,15 @@ discToken = os.environ.get('DISCORD_TOKEN')
 r = redis.from_url(os.environ.get("REDIS_URL"))
 
 ign_channels = [int(x) for x in r.lrange("ign_channels",0,-1)]
-print("hi")
 
 @client.event
 async def on_ready():
-    print("ready")
-    print(client.guilds)
     for guild in client.guilds:
         print(guild.name)
         for tch in guild.text_channels:
             print(tch.name)
             if tch.id not in ign_channels:
-                await tch.send("IT'S HIGH NOON'")
+                await tch.send("IT'S HIGH NOON")
                 await tch.send(imgurpackage.get_digest())
     keylist = r.keys("*/requests")
     reqMsg = ""
